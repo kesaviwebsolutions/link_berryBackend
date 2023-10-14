913e8a69da25207386e084c3ffdf0380ed18a47d
SAURABH YADAV <55534242+saurabh931@users.noreply.github.com>
2023-10-15 07:53:38 +0800

    main.py

--- python_scraper/profileAndAge.py
+++ python_scraper/profileAndAge.py
@@ -8,8 +8,11 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from flask_cors import CORS  # Import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 load_dotenv()
 
